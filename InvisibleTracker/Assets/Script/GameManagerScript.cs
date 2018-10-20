76df40a505ab12bb1020912054b028efca4c7fc7
Shumpei Akahoshi <xstarred@gmail.com>
2018-10-20 12:16:44 +0900

    Prefab/Script/Soundなど追加

---
+++ InvisibleTracker/Assets/Script/GameManagerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManagerScript : MonoBehaviour {
+
+	public bool gameend = true;
+
+	public float gameTime = 60;	//1ゲーム5分の長さ．5分くらいかなあ．
+	public int scoreA;
+	public int scoreB;
+
+	public Text TimeText;
+	public Text AscoreText;
+	public Text BscoreText;
+
+	public AudioSource GameManageAudio;	//ゲーム終了時に効果音を再生
+
+	void Start () {
+		GameManageAudio = gameObject.GetComponent<AudioSource> ();
+	}
+	
+	void Update () {
+		gameTime -= Time.deltaTime;	//時間経過で制限時間の残りを減らしていく
+		float timeTextTime = Mathf.Floor (gameTime);	//uGUI表示時に小数点以下を捨てる
+
+		//ゲームの残り時間がある限りuGUIを更新し続ける
+		if(gameTime > 0){
+			TimeText.text = "Time : " + timeTextTime.ToString ();
+			AscoreText.text = "Player A : " + scoreA.ToString();
+			BscoreText.text = "Player B : " + scoreB.ToString();
+		}
+		//制限時間が0以下になったらゲーム終了処理
+		if(gameTime < 0){
+			//制限時間がなくなってからずーっとリトライとか呼ばれ続けると面倒くさいのでboolを噛ます
+			if (gameend) {
+				GameManageAudio.Play (); //終了効果音
+				if (scoreA > scoreB) TimeText.text = "PlayerA WIN";	//PlayerAの方がスコアが高ければA勝利表示
+				if (scoreB > scoreA) TimeText.text = "PlayerB WIN";	//PlayerBの方が　同上
+				if (scoreB == scoreA) TimeText.text = "DRAW";		//プレイヤーのスコアが同じなら引き分け表示
+
+				//AscoreText.text = null;	
+				//BscoreText.text = null;
+				gameend = false;		//処理が重複しないようにboolを切る
+				Invoke ("Retry", 10f);	//再起処理　ここは手動でもいいのかも
+			}
+		}
+	}
+	public void Retry(){
+		SceneManager.LoadScene ("MainScene");
+	}
+}
---
+++ InvisibleTracker/Assets/Script/ObjectScript.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//オブジェクト自体が自分で管理する処理たち
+//衝突押下音再生，ゴール到達判定，ゲームマネージャーへの得点加算を担当
+
+//ぶつかったときの音源はオブジェクト自体が保有　オブジェクトの種類によって音を変えたいから
+//そのためトラッカーと衝突時の効果音はオブジェクトから再生する
+//あと衝突したトラッカーのプレイヤー判定はオブジェクトが行う　そのプレイヤに対してゴール時に得点加算する
+//ゴール判定も当たり判定を使って済ませたいのでオブジェクト側で行う
+//トラッカたープレイヤ
+
+public class ObjectScript : MonoBehaviour {
+
+	public AudioSource objectCollisionAudio;
+	public AudioClip[] objectCollisionClip;
+
+	public GameObject GameManager;
+
+	public int PlayerAscore;
+	public int PlayerBscore;
+
+	public int scorePlayer;
+
+	public GameManagerScript gameManagerScript;
+
+	void Start () {
+		GameManager = GameObject.Find ("GameManager");
+		objectCollisionAudio = this.GetComponent<AudioSource> ();
+		gameManagerScript = GameManager.GetComponent<GameManagerScript> ();
+	}
+	
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision col){
+		objectCollisionAudio.clip = objectCollisionClip[Random.Range (0,3)];
+		AudioSource.PlayClipAtPoint (objectCollisionAudio.clip, this.gameObject.transform.position);
+		if(col.gameObject.tag == "TrackerA"){
+			scorePlayer = 1;
+		}
+		if(col.gameObject.tag == "TrackerB"){
+			scorePlayer = 2;
+		}
+		//ゴールに入った処理
+		if(col.gameObject.tag == "Goal"){
+			Debug.Log ("Goalに衝突");
+			switch (scorePlayer) {
+			case 1:
+				gameManagerScript.scoreA += 10;
+				Destroy (gameObject);
+				break;
+			case 2:
+				gameManagerScript.scoreB += 10;
+				Destroy (gameObject);
+				break;
+			}
+		}
+	}
+}
